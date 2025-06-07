870b1ef565bbc88b21ad753700cced0685ad66c7
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-07 20:12:52 +0700

    Latihan Kotlin

--- src/main/kotlin/kondisional.kt
+++ src/main/kotlin/kondisional.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val umur = 18
+/*    val umur = 18
 
     if (umur >= 20) { // Lebih Dari atau sama dengan
         println("Anda Boleh Daftar Judol")
@@ -23,7 +23,47 @@ fun main() {
         absen >= 90 -> "A"
         absen >= 80 -> "B"
         else -> "C"
+    }*/
+
+    // practice
+
+    // Soal 2
+    val nilai = 85
+
+    if (nilai >= 85) {
+        println("Lulus dengan pujian")
+    } else if (nilai >= 60) {
+        println("Lulus")
+    } else {
+        println("Tidak Lulus")
     }
 
+    // Soal 3
+    val umur = 16
+    if (umur >= 17) {
+        println("Boleh memilih")
+    } else {
+        println("Belum Boleh memilih")
+    }
 
+    // Soal 4
+    val hari = "8"
+    when (hari) {
+        "1" -> println("Senin")
+        "2" -> println("Selasa")
+        "3" -> println("Rabu")
+        "4" -> println("Kamis")
+        "5" -> println("Jum'at")
+        "6" -> println("Sabtu")
+        "7" -> println("Minggu")
+        else -> println("Input Tidak Valid")
+    }
+
+    val warna = "Jingga"
+    val hasil = when (warna) {
+        "Merah" -> println("Bahaya")
+        "Hijau" -> println("Aman")
+        "Biru" -> println("Terang")
+        else -> println("Warna tidak valid")
+    }
 }
