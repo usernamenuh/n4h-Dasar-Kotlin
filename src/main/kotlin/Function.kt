6c11b28bbbc2e58aa9068058a9b27aa9e95c42e6
Usernamenuh <muhammadenuh4@gmail.com>
2025-06-08 00:16:06 +0700

    Latihan Kotlin

--- src/main/kotlin/Function.kt
+++ src/main/kotlin/Function.kt
@@ -1,8 +1,13 @@
 fun main() {
     sapa("Nuh")
+    val hasil = tambah(10,10)
+    println("Hasil = $hasil")
 }
 
 fun sapa(nama: String) {
     println("Hello $nama")
 }
 
+fun tambah(a: Int, b: Int): Int {
+    return a + b
+}
